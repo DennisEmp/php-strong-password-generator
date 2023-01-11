355d00249809e811c3120f89e321d41c4a8abb05
Dennis Empirio <dennis.empirio@gmail.com>
2023-01-11 12:50:11 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+<!-- Repo
+php-strong-password-generator
+
+Todo
+Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password sicure. L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale da restituire all’utente. La password dovra' essere composta da lettere minuscole e maiuscole, numeri e simboli
+Scriviamo tutto (logica e layout) in un unico file index.php.
+
+Milestone 2
+Verificato il corretto funzionamento del nostro codice. Spostiamo poi la logica in un file helper.php che includeremo poi nella pagina principale. -->
